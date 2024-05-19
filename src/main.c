ac01036d0d093308d17bdfe19b53039b998ed198
1gni5 <jules.ducange@posteo.net>
2024-05-19 17:55:18 +0200

    ✨ Handle insert collisions

--- src/main.c
+++ src/main.c
@@ -15,17 +15,23 @@ int hash_str(const char *str)
     }
     return hash;
 }
+typedef struct entry
+{
+    char *key;
+    char *value;
+} entry_t;
+
 typedef struct hashmap
 {
     int size;
-    char **buckets;
+    entry_t **buckets;
 } hashmap_t;
 
-hashmap_t *create_hashmap(int s)
+hashmap_t *create_hashmap(int size)
 {
     hashmap_t *hm = (hashmap_t *)malloc(sizeof(hashmap_t));
-    hm->size = s;
-    hm->buckets = (char **)malloc(hm->size * sizeof(hashmap_t));
+    hm->size = size;
+    hm->buckets = (entry_t **)malloc(hm->size * sizeof(entry_t));
     return hm;
 }
 
@@ -43,12 +49,23 @@ void delete_hashmap(hashmap_t *hm)
     free(hm);
 }
 
-void hashmap_insert(hashmap_t *hm, const char *value)
+int hashmap_insert(hashmap_t *hm, char *key, char *value)
 {
-    const int hash = hash_str(value);
-    const int index = hash % hm->size;
-    hm->buckets[index] = (char *)malloc(strlen(value));
-    strcpy(hm->buckets[index], value);
+    const int hash = hash_str(key);
+    int index = hash % hm->size;
+
+    // bucket not empty and different value -> collision
+    while (hm->buckets[index] != NULL && strcmp(hm->buckets[index]->key, key))
+    {
+        index = (index + 1) % hm->size;
+        if (index == (hash % hm->size))
+            return -1;
+    }
+    hm->buckets[index] = (entry_t *)malloc(sizeof(entry_t));
+    hm->buckets[index]->key = key;
+    hm->buckets[index]->value = value;
+
+    return 0;
 }
 
 void print_hashmap(const hashmap_t *hm)
@@ -57,15 +74,24 @@ void print_hashmap(const hashmap_t *hm)
     for (int i = 0; i < hm->size; i++)
     {
         bool is_last = i == hm->size - 1;
-        printf(is_last ? "%s" : "%s,", hm->buckets[i]);
+        if (hm->buckets[i] != NULL)
+        {
+            printf("%s->%s", hm->buckets[i]->key, hm->buckets[i]->value);
+        }
+        else
+        {
+            printf(" ");
+        }
+        if (!is_last)
+            printf(",");
     }
     printf("}\n");
 }
 
 int main()
 {
-    hashmap_t *hm = create_hashmap(10);
-    hashmap_insert(hm, "hello");
+    hashmap_t *hm = create_hashmap(3);
+    hashmap_insert(hm, "hello", "world");
     print_hashmap(hm);
     delete_hashmap(hm);
     return 0;
